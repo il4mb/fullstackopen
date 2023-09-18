f689443b016b93dd0e25b00877ad749ef8cfef7a
il4mb <durianbohong@gmail.com>
2023-09-18 15:48:28 +0700

    modified:   part1/counter/src/App.js 	modified:   part1/counter/src/index.js 	deleted:    part1/exercises1a_/.gitignore 	deleted:    part1/exercises1a_/README.md 	deleted:    part1/exercises1a_/package-lock.json 	deleted:    part1/exercises1a_/package.json 	deleted:    part1/exercises1a_/public/favicon.ico 	deleted:    part1/exercises1a_/public/index.html 	deleted:    part1/exercises1a_/public/logo192.png 	deleted:    part1/exercises1a_/public/logo512.png 	deleted:    part1/exercises1a_/public/manifest.json 	deleted:    part1/exercises1a_/public/robots.txt 	deleted:    part1/exercises1a_/src/App.css 	deleted:    part1/exercises1a_/src/App.js 	deleted:    part1/exercises1a_/src/App.test.js 	deleted:    part1/exercises1a_/src/index.css 	deleted:    part1/exercises1a_/src/index.js 	deleted:    part1/exercises1a_/src/logo.svg 	deleted:    part1/exercises1a_/src/reportWebVitals.js 	deleted:    part1/exercises1a_/src/setupTests.js

--- part1/counter/src/App.js
+++ part1/counter/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+
+const Display = ({ counter }) => <div>{counter}</div>;
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1)
+  };
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  };
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Display counter={counter} />
+      <Button text="add" handleClick={increaseByOne} />
+      <Button text="reset" handleClick={setToZero} />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   );
 }
